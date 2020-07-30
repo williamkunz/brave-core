b343de52b0b82ede5fd9f7c3a870a0468fd81999
Pranjal Jumde <pranjal@brave.com>
2020-07-30 12:07:30 -0700

    SetSeed works now

--- vendor/brave-ios/components/Passphrase.mm
+++ vendor/brave-ios/components/Passphrase.mm
@@ -3,8 +3,12 @@
 #include <string>
 #include <vector>
 
-#include "brave/components/brave_sync/crypto/crypto.h"
 #include "base/strings/sys_string_conversions.h"
+#include "brave/components/brave_sync/brave_sync_prefs.h"
+#include "brave/components/brave_sync/crypto/crypto.h"
+#include "brave/ios/browser/browser_state/browser_state_manager.h"
+#include "components/prefs/pref_service.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 
 @interface PassPhrase()
 {
@@ -14,8 +18,10 @@
 
 @implementation PassPhrase
 - (NSString *)getSyncCode {
+	brave_sync::Prefs brave_sync_prefs{BrowserStateManager::GetInstance().GetBrowserState()->GetPrefs()};
 	std::vector<uint8_t> seed = brave_sync::crypto::GetSeed();
     std::string sync_code = brave_sync::crypto::PassphraseFromBytes32(seed);
+    brave_sync_prefs.SetSeed(sync_code);
     return base::SysUTF8ToNSString(sync_code.c_str());
 }
 @end
